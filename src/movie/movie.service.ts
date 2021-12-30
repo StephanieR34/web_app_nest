763ca20b17ff57b5f43f8afa13c96921163c175c
StephanieR34 <34.regnier.stephanie@gmail.com>
2021-12-30 13:48:49 +0100

    module movie

---
+++ src/movie/movie.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import {  HttpStatus, HttpException } from '@nestjs/common';
+
+
+
+export interface Movie {
+  id: number;
+  name: string;
+  year: number;
+}
+
+
+@Injectable()
+export class MovieService {
+  private movies: Movie[] = [
+    { id: 1, name: 'Star Wars: The Force Awakens', year: 2015 },
+    { id: 2, name: 'Star Wars: The Last Jedi', year: 2017 },
+    { id: 3, name: 'Star Wars: The Rise of Skywalker', year: 2019 },
+    { id: 4, name: 'SOS fantomes', year: 2021 },
+
+  ];
+
+  getMovies(): Movie[] {
+    return this.movies;
+  }
+
+
+  getById(id: number): Movie {
+    const movie = this.movies[id-1];
+    if (movie)
+      {
+      return movie;}
+    throw new HttpException(`Le movie d'id ${id} n'existe pas` , HttpStatus.NOT_FOUND)
+    // {
+    //   (`Le movie d'id ${id} n'existe pas`)}
+  }
+}
+
+
