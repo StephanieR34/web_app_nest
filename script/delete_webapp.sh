45fdba3b3265d4b0c407c0f2209ab104be0e09aa
StephanieR34 <34.regnier.stephanie@gmail.com>
2022-01-24 11:52:26 +0100

    mise a jour

--- script/delete_webapp.sh
+++ script/delete_webapp.sh
@@ -4,6 +4,6 @@ echo "delete web app Azure : $WebAppName"
 az webapp delete --name $WebAppName --resource-group $ResourceGroup 
 
 echo "delete appservice plan"
-az appservice plan delete --name $AppServiceName --resource-group $ResourceGroup
+az appservice plan delete --name $AppServiceName --resource-group $ResourceGroup -y
 
 source script/delete_var.sh
